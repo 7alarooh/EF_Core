5795de3f00f13f55911c33fb7134eb7949c7cbb4
Azzak <azzaalmahrooqi79@gmail.com>
2024-11-05 16:12:13 +0400

    create Project class with validations and keys

---
+++ OutsystemCompany/Models/Project.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutsystemCompany.Models
+{
+    public class Project
+    {
+        [Key]
+        public int Pnumber { get; set; }
+
+        [Required(ErrorMessage = "Project name is required.")]
+        [StringLength(100, ErrorMessage = "Project name cannot be longer than 100 characters.")]
+        public string Pname { get; set; }
+
+        [StringLength(200, ErrorMessage = "Project location cannot be longer than 200 characters.")]
+        public string Plocation { get; set; }
+
+        [ForeignKey("Department")]
+        public int Dnum { get; set; }
+        public virtual Department Department { get; set; }
+
+    }
+}
