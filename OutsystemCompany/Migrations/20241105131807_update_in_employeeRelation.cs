cac2fed5da7c98784b741066876c211afa0c3448
Azzak <azzaalmahrooqi79@gmail.com>
2024-11-05 17:31:18 +0400

    Build Database and add migrations

---
+++ OutsystemCompany/Migrations/20241105131807_update_in_employeeRelation.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OutsystemCompany.Migrations
+{
+    /// <inheritdoc />
+    public partial class update_in_employeeRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ OutsystemCompany/Migrations/20241105132349_update_in_DataType_in_EmployeeRelation.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OutsystemCompany.Migrations
+{
+    /// <inheritdoc />
+    public partial class update_in_DataType_in_EmployeeRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
