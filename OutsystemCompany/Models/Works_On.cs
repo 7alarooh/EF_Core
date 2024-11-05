702abceecf025a635ba26c06d53e9d6b405e0163
Azzak <azzaalmahrooqi79@gmail.com>
2024-11-05 16:17:52 +0400

    create works_on class with validations and keys

---
+++ OutsystemCompany/Models/Works_On.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutsystemCompany.Models
+{
+    [PrimaryKey(nameof(Essn), nameof(Pno))]
+    public class Works_On
+    {
+        [ForeignKey("Employee")]
+        [Required(ErrorMessage = "Employee SSN is required.")]
+        public int Essn { get; set; }
+        public virtual Employee Employee { get; set; }
+
+        [ForeignKey("Project")]
+        [Required(ErrorMessage = "Project number is required.")]
+        public int Pno { get; set; }
+        public virtual Project Project { get; set; }
+
+        [Range(0, 168, ErrorMessage = "Hours must be between 0 and 168.")]
+        public decimal Hours { get; set; } 
+    }
+}
