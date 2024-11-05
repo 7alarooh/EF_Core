ae03449a6a9f55283329dcd880e645ad7e281bff
Azzak <azzaalmahrooqi79@gmail.com>
2024-11-05 16:08:02 +0400

    create department location class with validations and keys

---
+++ OutsystemCompany/Models/Depat_Location.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutsystemCompany.Models
+{
+    [PrimaryKey(nameof(Dnumber), nameof(Dlocation))]
+    public class DeptLocation
+    {
+        [ForeignKey("Department")]
+        public int Dnumber { get; set; }
+
+        public virtual Department Department { get; set; }
+
+        [Required(ErrorMessage = "Location is required.")]
+        [StringLength(100, ErrorMessage = "Location cannot be longer than 100 characters.")]
+        public string Dlocation { get; set; }
+    }
+}
