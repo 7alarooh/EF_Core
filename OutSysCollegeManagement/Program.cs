602f3e45bc6a637b6388146d4b1de67ec5a93627
Azzak <azzaalmahrooqi79@gmail.com>
2024-11-06 09:55:25 +0400

    Start new Project for College System

---
+++ OutSysCollegeManagement/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OutSysCollegeManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
