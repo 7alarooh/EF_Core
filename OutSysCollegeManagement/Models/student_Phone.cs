24b5f1a8c85848104cd6e4765050aa2ef53a4134
Azzak <azzaalmahrooqi79@gmail.com>
2024-11-06 11:39:44 +0400

    create class for Student Phone

---
+++ OutSysCollegeManagement/Models/student_Phone.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace OutSysCollegeManagement.Models
+{
+    [PrimaryKey(nameof(SID), nameof(Phone_no))]
+    public class student_Phone
+    {
+        //student id
+        [ForeignKey("Student")]
+        public int SID { get; set; }
+        // Navigation property for the Student reference
+        public virtual Student Student { get; set; }
+        // phone number
+        [Required]
+        [RegularExpression(@"^\d{8}$", ErrorMessage = "Phone number must be exactly 8 digits.")]
+        public string Phone_no { get; set; }
+    }
+}
