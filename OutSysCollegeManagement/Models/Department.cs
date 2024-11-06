4e24c77cb8ce3a75601ef8ad636879fd6462ecf8
Azzak <azzaalmahrooqi79@gmail.com>
2024-11-06 12:09:19 +0400

    create department class and validation

---
+++ OutSysCollegeManagement/Models/Department.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutSysCollegeManagement.Models
+{
+    public class Department
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Department_id { get; set; }
+
+        [Required(ErrorMessage = "Department name is required.")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Department name must be between 2 and 100 characters.")]
+        [RegularExpression(@"^[A-Za-z\s]+$", ErrorMessage = "Department name can only contain letters and spaces.")]
+        public string D_name { get; set; }
+    }
+}
