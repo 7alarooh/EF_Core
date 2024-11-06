d6e609e0b199a292f2784abe6b68aa85b98fc921
Azzak <azzaalmahrooqi79@gmail.com>
2024-11-06 10:12:38 +0400

    add ApplicationDbContext class

---
+++ OutSysCollegeManagement/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutSysCollegeManagement
+{
+    public class ApplicationDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(" Data Source=Localhost; Initial Catalog=OutSysCollegeManagement; Integrated Security=true; TrustServerCertificate=True ");
+        }
+        //public DbSet<Employee> Employees { get; set; }
+    }
+}
