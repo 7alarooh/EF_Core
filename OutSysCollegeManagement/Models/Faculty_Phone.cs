4a1ff29bf531541e20a16110301bea0e534e3bf2
Azzak <azzaalmahrooqi79@gmail.com>
2024-11-06 12:44:34 +0400

    create Faculty_Phone  class with adding PK ,FK, and validations

---
+++ OutSysCollegeManagement/Models/Faculty_Phone.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutSysCollegeManagement.Models
+{
+    
+    [PrimaryKey(nameof(SID), nameof(Mobile_no))]
+    public class Faculty_Phone
+    {
+        //student id
+        [ForeignKey("Student")]  
+        public int SID { get; set; }
+        public virtual Student Student { get; set; } // Navigation 
+
+        // phone number
+        [Required(ErrorMessage = "Mobile number is required.")]
+        [StringLength(15, ErrorMessage = "Mobile number cannot exceed 15 characters.")]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Mobile number must be exactly 10 digits.")]
+        public string Mobile_no { get; set; }  // Mobile number
+    }
+}
