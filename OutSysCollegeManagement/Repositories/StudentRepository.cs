b95a973428cbad0d945a37287d4354de10439ed0
Azzak <azzaalmahrooqi79@gmail.com>
2024-11-10 14:45:11 +0400

    create Repositories folder, and add the first Repository for student

---
+++ OutSysCollegeManagement/Repositories/StudentRepository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OutSysCollegeManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutSysCollegeManagement.Repositories
+{
+    public class StudentRepository
+    {
+        private readonly CollegeDbContext _context;
+
+        public StudentRepository(CollegeDbContext context)
+        {
+            _context = context;
+        }
+        // GetAllStudents: Retrieve a list of all students with related courses, exams, and hostel information
+        public async Task<List<Student>> GetAllStudents()
+        {
+            return await _context.Students
+                .Include(s => s.Courses)
+                .Include(s => s.Hostel)
+                .Include(s => s.Faculty)
+                .ToListAsync();
+        }
+
+    }
+}
